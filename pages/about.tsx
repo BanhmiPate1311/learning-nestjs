b88dcbb7cee676812f33b13aea5cabec81b370f7
huynhcuong <huynhcuong@gmail.com>
2023-09-08 17:59:52 +0700

    Automatic Static Optimization

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import { useRouter } from 'next/router'
+import React, { useEffect } from 'react'
 // tsdrp
 
-export interface AboutPageProps {
+export interface AboutPageProps {}
+
+export default function AboutPage(props: AboutPageProps) {
+  const router = useRouter()
+
+  console.log('About query: ', router.query)
+
+  useEffect(() => {
+    // Check query available
+  }, [])
+
+  //JSON.parse($0.text)
+
+  return <div>About Page</div>
 }
 
-export default function AboutPage (props: AboutPageProps) {
-  return (
-    <div>
-      About Page
-    </div>
-  );
+export async function getServerSideProps() {
+  return {
+    props: {}, // will be passed to the page component as props
+  }
 }
