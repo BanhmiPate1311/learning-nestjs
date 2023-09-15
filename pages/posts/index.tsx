72ebfd23712e2d78c75a796d084a2b30a1784ed7
huynhcuong <huynhcuong@gmail.com>
2023-09-15 18:02:41 +0700

    getStatisProp

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,8 +1,38 @@
-import * as React from 'react';
+import { GetStaticProps, GetStaticPropsContext } from 'next'
+import * as React from 'react'
 // tsdrp
 
-export interface PostListProps {}
+export interface PostListPageProps {
+  posts: any[]
+}
 
-export default function PostList(props: PostListProps) {
-  return <div>Post List Page</div>;
+export default function PostListPage({ posts }: PostListPageProps) {
+  console.log('post: ', posts)
+  return (
+    <div>
+      <h1>Post List Page</h1>
+
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export const getStaticProps: GetStaticProps<PostListPageProps> = async (
+  context: GetStaticPropsContext,
+) => {
+  // server-side
+  // build-time
+  console.log('static props')
+  const response = await fetch('https://js-post-api.herokuapp.com/api/posts?_page=1')
+  const data = await response.json()
+  console.log('data: ', data)
+  return {
+    props: {
+      posts: data.data.map((x: any) => ({ id: x.id, title: x.title })), // cắt bớt dữ liệu, chỉ lấy id và title
+    },
+  }
 }
